00db21ec56150c30aa2c2de56c561522a1438c7b
HARSH DOIPHODE <98657818+HARSHDOIPHODE@users.noreply.github.com>
2024-01-02 18:35:32 +0530

    Create token.sol

---
+++ token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract myToken is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("MyToken", "MT") Ownable(initialOwner) {
+        _mint(msg.sender, 100000);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
+}
